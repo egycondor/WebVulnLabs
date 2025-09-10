d37cc6b6dbb2f98a56e7350d75ac66dd9cb5864d
egycondor <eng_ahmed.mohsen@live.com>
2025-09-10 08:38:39 +0400

    rmlab6

--- lab06/db/init.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `name` TEXT NOT NULL,
-  `password` TEXT NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-INSERT INTO `users` VALUES ('Looooooolz','Lolz !!');
