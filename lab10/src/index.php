375733d98d10f3adfffd2116c9ce703438913d88
egycondor <eng_ahmed.mohsen@live.com>
2025-09-11 22:34:08 +0400

    111

--- lab10/src/index.php
+++ lab10/src/index.php
@@ -10,7 +10,6 @@
     <a href="/import-hardened.php">Local Import (Hardened)</a>
     <a href="/import-network.php">Network Import (XXE/SSRF)</a>
     <a href="/import-upload.php">Upload Import (XXE)</a>
-    <a href="/import-expect.php">Expect Import (RCE)</a>
   </div>
 </div></div>
 <?php theme_foot(); ?>
