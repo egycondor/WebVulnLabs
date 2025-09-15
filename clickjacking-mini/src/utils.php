431281bbecfa63955c9d24b542678de0aae3b67f
egycondor <eng_ahmed.mohsen@live.com>
2025-09-16 02:40:16 +0400

    d

--- clickjacking-mini/src/utils.php
+++
@@ -1,3 +0,0 @@
-<?php
-function start_lab_session(){ if(session_status()!==PHP_SESSION_ACTIVE){ session_start(); } }
-function require_login(){ start_lab_session(); if(empty($_SESSION['user'])){ header('Location: /login.php'); exit; } }
